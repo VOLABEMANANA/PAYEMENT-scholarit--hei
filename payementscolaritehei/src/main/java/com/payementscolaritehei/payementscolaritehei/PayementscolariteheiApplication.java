835dd26eb7a144dcff4f4af1e46434b5886536ee
VOLABEMANANA <hei.fabricia.2@gmail.com>
2022-09-13 14:57:42 +0300

    initial commit

---
+++ payementscolaritehei/src/main/java/com/payementscolaritehei/payementscolaritehei/PayementscolariteheiApplication.java
@@ -0,0 +1,13 @@
+package com.payementscolaritehei.payementscolaritehei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayementscolariteheiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayementscolariteheiApplication.class, args);
+	}
+
+}
---
+++ payementscolaritehei/src/test/java/com/payementscolaritehei/payementscolaritehei/PayementscolariteheiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payementscolaritehei.payementscolaritehei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayementscolariteheiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
