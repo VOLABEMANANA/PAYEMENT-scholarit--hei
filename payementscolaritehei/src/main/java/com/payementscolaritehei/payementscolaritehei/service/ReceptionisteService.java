b29f6e951fdd4b8e7f67b349187308039718fd12
VOLABEMANANA <103513576+VOLABEMANANA@users.noreply.github.com>
2022-09-14 19:06:14 +0300

    Update ReceptionisteService.java

--- payementscolaritehei/src/main/java/com/payementscolaritehei/payementscolaritehei/service/ReceptionisteService.java
+++ payementscolaritehei/src/main/java/com/payementscolaritehei/payementscolaritehei/service/ReceptionisteService.java
@@ -3,10 +3,10 @@ package com.payementscolaritehei.payementscolaritehei.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.payementscolaritehei.payementscolaritehei.repository.StudentRepository;
+import com.payementscolaritehei.payementscolaritehei.repository.ReceptionisteRepository;
 
 @Service
 public class ReceptionisteService {
    @Autowired
-   private StudentRepository studentRepository;
+   private ReceptionisteRepository receptionisteRepository;
 }
