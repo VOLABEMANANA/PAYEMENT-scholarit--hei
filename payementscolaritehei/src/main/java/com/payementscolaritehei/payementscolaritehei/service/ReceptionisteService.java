6c29f30c4ecaaf3fde22cc8b40972e87caf93f33
VOLABEMANANA <103513576+VOLABEMANANA@users.noreply.github.com>
2022-09-14 19:24:52 +0300

    feat: add & update

--- payementscolaritehei/src/main/java/com/payementscolaritehei/payementscolaritehei/service/ReceptionisteService.java
+++ payementscolaritehei/src/main/java/com/payementscolaritehei/payementscolaritehei/service/ReceptionisteService.java
@@ -1,12 +1,36 @@
 package com.payementscolaritehei.payementscolaritehei.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.payementscolaritehei.payementscolaritehei.repository.ReceptionisteRepository;
+import com.payementscolaritehei.payementscolaritehei.model.Receptioniste;
+
+import Lombok.AllAgrsConstructor
 
 @Service
+@AllArgsConstructor
 public class ReceptionisteService {
    @Autowired
    private ReceptionisteRepository receptionisteRepository;
+
+   public List<Receptioniste> getAllReceptionistes(){
+      return receptionisteRepository.findAll();
+   }
+
+   public Optional<Receptioniste> getReceptioniste(Integer id){
+      return receptionisteRepository.findById(id);
+   }
+
+   public Receptioniste addReceptioniste(Receptioniste receptioniste){
+      return receptionisteRepository.save(receptioniste);
+   }
+
+   public void deleteReceptioniste(Integer id){
+      return receptionisteRepository.deleteById(id);
+   }
+
 }
